522156f8456c0ec08f62e44ace87cf7fc4dafe32
PoptamasVlad <118806771+PoptamasVlad@users.noreply.github.com>
2022-11-25 21:00:48 +0200

    Add files via upload

---
+++ lab1ex8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab1ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Se citesc doua numere de la tastatura, x si y. Scrieti un program care va verifica
+            daca x este divizibil cu y.
+            In cazul in care x este divizibil cu y, programul va afisa “divizibil” iar in caz contrar,
+            “indivizibil”*/
+
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+            if (x % y == 0)
+            {
+                Console.WriteLine("divizibil");
+            }
+            else
+            {
+                Console.WriteLine("indivizibil");
+            }
+        }
+    }
+}
